f439adbe78a5dbf577292cef9b831555ed17a688
Alexander Schramm <alexanderschramm@me.com>
2016-06-08 11:29:23 +0200

    Leap

---
+++ ruby/leap/leap.rb
@@ -0,0 +1,18 @@
+module BookKeeping
+  VERSION = 2
+end
+
+# Implements a check to test if a Year is a Leap Year.
+class Year
+  def self.leap?(year)
+    if year % 400 == 0
+      true
+    elsif year % 100 == 0
+      false
+    elsif year % 4 == 0
+      true
+    else
+      false
+    end
+  end
+end
