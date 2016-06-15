ea7800342479bc23d5f579f3a8cb4ff990c8da11
Alexander Schramm <alexanderschramm@me.com>
2016-06-15 14:58:13 +0200

    add enum for accumulate

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,6 +1,8 @@
 # Implements accumulate function for Arrays.
 class Array
   def accumulate
+    return to_enum(:accumulate) unless block_given?
+
     acc = []
     each do |item|
       acc << yield(item)
