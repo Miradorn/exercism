d7e035f8472b728386a8d69739119549dde37f1a
Alexander Schramm <alexanderschramm@me.com>
2016-06-29 16:00:02 +0200

    Nicer Value Array for Hex

--- ruby/hexadecimal/hexadecimal.rb
+++ ruby/hexadecimal/hexadecimal.rb
@@ -1,11 +1,9 @@
 # Implements Base16 to Base10 conversion
 class Hexadecimal
-  VALUE = ('0'..'9').map { |l| [l, l.to_i] }.to_h.merge('a' => 10,
-                                                        'b' => 11,
-                                                        'c' => 12,
-                                                        'd' => 13,
-                                                        'e' => 14,
-                                                        'f' => 15).freeze
+  VALUE = '0123456789abcdef'.each_char
+                            .each_with_index
+                            .map { |l, v| [l, v] }.to_h.freeze
+
   def initialize(value)
     @value = value
   end
